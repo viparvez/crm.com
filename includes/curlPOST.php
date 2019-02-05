72d76004b9edafee39cd058a6b8a82b27ea05813
Sirajum Monir Parvez <viparvez@gmail.com>
2019-02-05 17:23:28 +0600

    Create curlPOST.php
    Created curlPOST function

---
+++ includes/curlPOST.php
@@ -0,0 +1,42 @@
+<?php
+
+//=======================SET THE API USER CREDENTIALS========================
+$apiUrl = "https://202.84.39.107/crmapi/rest/v2/";
+$apiUsername = "sirajum.monir";
+$apiPassword = "P5KPL-AM";
+$apiOrganisation = "BEXIMCO";
+
+// ============================== Functions ==================================
+//==============================callCURL()=============================
+
+function curlPOST($data,$actionURL){
+	
+	global $apiUrl;
+	
+	$jsonData = json_encode($data);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,"$apiUrl$actionURL"); 
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS,$jsonData);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	                'Content-Type: application/json',
+	                'Content-Length: ' . strlen($jsonData))
+	        );
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$server_output = curl_exec ($ch);
+
+	if ($server_output === FALSE) {
+	    printf("cUrl error (#%d): %s<br>\n", curl_errno($ch),
+	    htmlspecialchars(curl_error($ch)));
+	}
+
+	$server_output = json_decode($server_output);
+
+	curl_close ($ch);
+	
+	return $server_output;
+}
