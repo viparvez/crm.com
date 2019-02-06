a15e1394704bfa420b0ba977b6516f8e3cf69ae1
Sirajum Monir Parvez <viparvez@gmail.com>
2019-02-06 14:19:46 +0600

    Loggin bulk uploads

--- config/config.db.php
+++ config/config.db.php
@@ -1,9 +1,12 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "P5KPL-AM";
+$dbname = "test";
 
-$con = mysqli_connect("127.0.0.1","root","P5KPL-AM","test");
-
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (mysqli_connect_errno())
-{
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 <body>
 	<form name="bulk_payment" method="POST" action="" enctype="multipart/form-data">
 		<input type="file" name="fileToUpload">
+		<select name="action">
+			<option value="payment">Payment Only</option>
+			<option value="payment and activation">Payment and Activation</option>
+		</select>
 		<input type="submit" name="submit" value="SUBMIT">
 	</form>
 </body>
@@ -31,22 +35,82 @@
 
 			$rows = explode(',', file_get_contents($fileName));
 
+			require 'config/config.db.php';
+
 			$token = getToken();
 
 			foreach ($rows as $key => $value) {
-				//Payment
-				$paymentInfo = explode(" ", $value);
-				$accrecnum = trim($paymentInfo[0]);
-				$amount = trim($paymentInfo[1]);
-				$pay = createPayment($accrecnum,$amount,'Cash', $token);
-				//Activation
-				$status= checkStatus($accrecnum);
-				$sub_id = "S0".get_string_between($status,"S0","\"");
-				$activate = activate($sub_id);
 
-				echo $activate." account number: $accrecnum</br>";
+				$action = $_POST['action'];
+
+				if ($action == 'payment') {
+
+					//Payment
+					$paymentInfo = explode(" ", $value);
+					$accrecnum = trim($paymentInfo[0]);
+					$amount = trim($paymentInfo[1]);
+					$pay = createPayment($accrecnum,$amount,'Cash', $token);
+
+					log_payment($pay, $accrecnum, $action);
+
+				} else if ($_POST['action'] == 'payment and activation') {
+					//Payment
+					$paymentInfo = explode(" ", $value);
+					$accrecnum = trim($paymentInfo[0]);
+					$amount = trim($paymentInfo[1]);
+					$pay = createPayment($accrecnum,$amount,'Cash', $token);
+
+					//Activation
+					$status= checkStatus($accrecnum);
+					$sub_id = "S0".get_string_between($status,"S0","\"");
+					$activate = activate($sub_id);
+
+					log_activation($activate, $accrecnum, $action);
+				}
 			}
 		}
 
+	}
+
+	function log_payment($pay, $accrecnum, $action) {
+		
+		global $conn;
+		
+		$dateTime = date("Y-m-d h:i:s");
+
+		if ($pay == 'Payment Posted') {
+			$sql = "INSERT INTO testlogger (accrecnum, action, success, message, created_at)
+			VALUES ('$accrecnum', '$action', 'YES', '$pay', '$dateTime')";
+		} else {
+			$sql = "INSERT INTO testlogger (accrecnum, action, success, message, created_at)
+			VALUES ('$accrecnum', '$action', 'NO', '$pay', '$dateTime')";
+		}
+
+		if ($conn->query($sql) === TRUE) {
+		    echo "All jobs executed </br>";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error."</br>";
+		}
+	}
+
+	function log_activation($activate, $accrecnum, $action) {
+
+		global $conn;
+
+		$dateTime = date("Y-m-d h:i:s");
+
+		if ($activate == 'Activation Completed') {
+			$sql = "INSERT INTO testlogger (accrecnum, action, success, message, created_at)
+			VALUES ('$accrecnum', '$action', 'YES', '$activate', '$dateTime')";
+		} else {
+			$sql = "INSERT INTO testlogger (accrecnum, action, success, message, created_at)
+			VALUES ('$accrecnum', '$action', 'NO', '$activate', '$dateTime')";
+		}
+
+		if ($conn->query($sql) === TRUE) {
+		    echo "All jobs executed </br>";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error. "</br>";
+		}
 	}
 ?>
